9e11cbed5c3969331fd1e790b692b63bcd6fbb0d
Dmitry Elagin <elagin.dmitry2018@yandex.ru>
2019-12-11 17:26:06 +0300

    Добавлен метод для решения задачи по проверке соответствия строки шаблону телефонного номера

--- src/com/dmitrii/elagin/PhoneNumber.java
+++ src/com/dmitrii/elagin/PhoneNumber.java
@@ -1,9 +1,19 @@
 package com.dmitrii.elagin;
 
+import java.util.regex.Pattern;
+
 public class PhoneNumber {
 
     public static String createPhoneNumber(int[] nums) {
         return String.format("(%d%d%d) %d%d%d-%d%d%d%d", nums[0],nums[1],nums[2],nums[3],nums[4],
                 nums[5],nums[6],nums[7],nums[8],nums[9]);
     }
+
+public  static boolean validPhoneNumber(String s) {
+
+
+   return Pattern.matches("/(\\d\\d\\d/)",s);
+
+}
+
 }
