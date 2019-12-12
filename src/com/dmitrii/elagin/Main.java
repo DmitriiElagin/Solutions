7e96fdf0cee836816b6fc024c11a3b66c3296506
Dmitry Elagin <elagin.dmitry2018@yandex.ru>
2019-12-12 11:29:43 +0300

    Класс Utils переименован в Solutions

--- src/com/dmitrii/elagin/Main.java
+++ src/com/dmitrii/elagin/Main.java
@@ -5,12 +5,13 @@ public class Main {
     public static void main(String[] args) {
 
         boolean valid=CardNumberValidator.validate("2202201919306122");
-
         System.out.println(valid);
+
         System.out.println(PhoneNumber.createPhoneNumber(new int[]{1,2,3,4,5,6,7,8,9,0}));
+
         System.out.println(PhoneNumber.validPhoneNumber("(123) 456-7890"));
 
-        int[] ints = Utils.twoSum(new int[]{3, 5, 7,-3,18,123}, 130);
+        int[] ints = Solutions.twoSum(new int[]{3, 5, 7,-3,18,123}, 130);
 
         System.out.println(ints[0]+", " +ints[1]);
     }
--- src/com/dmitrii/elagin/Solutions.java
+++ src/com/dmitrii/elagin/Solutions.java
@@ -1,12 +1,11 @@
 package com.dmitrii.elagin;
 
-public class Utils {
+public class Solutions {
 
     //Находит индексы 2-х элементов массива, суммой которых является target
     public static int[] twoSum(int [] numbers, int target) {
 
     for(int i=0; i<numbers.length; i++) {
-
         for(int j=i+1; j<numbers.length; j++) {
             if(numbers[i]+numbers[j]==target) {
                 return  new int[]{i,j};
