422f22ba180d27d6d79c17d3c82f2ad77af04b9f
Dmitry Elagin <elagin.dmitry2018@yandex.ru>
2019-12-12 11:21:01 +0300

    Добавлен метод для решения задачи по поиску 2-х элементов массива, суммой которых является  аргумент метода

--- src/com/dmitrii/elagin/Main.java
+++ src/com/dmitrii/elagin/Main.java
@@ -9,5 +9,9 @@ public class Main {
         System.out.println(valid);
         System.out.println(PhoneNumber.createPhoneNumber(new int[]{1,2,3,4,5,6,7,8,9,0}));
         System.out.println(PhoneNumber.validPhoneNumber("(123) 456-7890"));
+
+        int[] ints = Utils.twoSum(new int[]{3, 5, 7,-3,18,123}, 130);
+
+        System.out.println(ints[0]+", " +ints[1]);
     }
 }
