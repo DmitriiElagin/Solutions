15f41ab3fd6f484e256de813569383c05c7e4743
Dmitry Elagin <elagin.dmitry2018@yandex.ru>
2019-12-25 14:16:16 +0300

    В Solutions.java добавлен метод stripComments

--- src/com/dmitrii/elagin/Main.java
+++ src/com/dmitrii/elagin/Main.java
@@ -4,15 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        boolean valid=CardNumberValidator.validate("2202201919306122");
-        System.out.println(valid);
+        //boolean valid=CardNumberValidator.validate("2202201919306122");
+        //System.out.println(valid);
 
-        System.out.println(PhoneNumber.createPhoneNumber(new int[]{1,2,3,4,5,6,7,8,9,0}));
+        //System.out.println(PhoneNumber.createPhoneNumber(new int[]{1,2,3,4,5,6,7,8,9,0}));
 
-        System.out.println(PhoneNumber.validPhoneNumber("(123) 456-7890"));
+        //System.out.println(PhoneNumber.validPhoneNumber("(123) 456-7890"));
 
-        int[] ints = Solutions.twoSum(new int[]{3, 5, 7,-3,18,123}, 130);
+        //int[] ints = Solutions.twoSum(new int[]{3, 5, 7,-3,18,123}, 130);
 
-        System.out.println(ints[0]+", " +ints[1]);
+        //System.out.println(ints[0]+", " +ints[1]);
+
+        String text="a &b \nc \nd ! e ";
+
+
+        System.out.println(Solutions.stripComments(text,new String[]{"!","&"}));
     }
 }
--- src/com/dmitrii/elagin/Solutions.java
+++ src/com/dmitrii/elagin/Solutions.java
@@ -1,5 +1,9 @@
 package com.dmitrii.elagin;
 
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Solutions {
 
     //Находит индексы 2-х элементов массива, суммой которых является target
@@ -15,5 +19,12 @@ public class Solutions {
 
     return null;
 }
+//Удаляет весь текст, закомментированный  указанными в аргументе символами
+public static String stripComments(String text, String[] commentSymbols) {
+    String symbols = String.format("[ ]*([%s].*)?$", String.join("", commentSymbols));
+    return Stream.of(text.split("\n")).map(t -> t.replaceAll(symbols, ""))
+            .collect(Collectors.joining("\n"));
+    }
+
 }
 
